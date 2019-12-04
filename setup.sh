588f52db8ce2387ee740e0a014ead383e4fe0322
hi0711 <admin@hi0711.xyz>
2019-12-04 15:47:46 +0900

    feat: setup.shのアップデート

--- setup.sh
+++ setup.sh
@@ -67,15 +67,17 @@ do
   [[ ${f} = "init.vim" ]] && continue
   [[ ${f} = "dein.toml" ]] && continue
   [[ ${f} = "dein_lazy.toml" ]] && continue
+  [[ ${f} = ".Brewfile" ]] && continue
 
   ln -snfv ${DOT_DIRECTORY}/${f} ${HOME}/${f}
 done
 
 if [ ! -d ${NEOVIM_DIRECTORY} ] ; then
   mkdir -p ${NEOVIM_DIRECTORY}/backup ${NEOVIM_DIRECTORY}/swap ${NEOVIM_DIRECTORY}/undo
-  ln -fv ${HOME}/dotfiles/init.vim ${HOME}/.config/nvim
-  ln -fv ${HOME}/dotfiles/dein.toml ${HOME}/.config/nvim
-  ln -fv ${HOME}/dotfiles/dein_lazy.toml ${HOME}/.config/nvim
+  cp -fv ${HOME}/dotfiles/init.vim ${HOME}/.config/nvim
+  cp -fv ${HOME}/dotfiles/dein.toml ${HOME}/.config/nvim
+  cp -fv ${HOME}/dotfiles/dein_lazy.toml ${HOME}/.config/nvim
+  cp -fv ${HOME}/dotfiles/.Brewfile ${HOME}/.Brewfile
 fi
 echo "$(tput setaf 2)Deploy dotfiles complete! :)$(tput sgr0)"
 
@@ -106,8 +108,40 @@ fi
 echo "$password" | sudo -S sh -c 'echo "/usr/local/bin/zsh" >> /etc/shells'
 echo "$password" | sudo -S chsh -s /usr/local/bin/zsh
 echo "$(tput setaf 2)Initialize zsh settings complete! :)$(tput sgr0)"
-# key repeat
-defaults write -g KeyRepeat -int 1
-defaults write -g InitialKeyRepeat -int 9
+# defaults settings
+defaults write -g NSAutomaticWindowAnimationsEnabled -bool false
+defaults write -g NSDocumentRevisionsWindowTransformAnimation -bool false
+defaults write -g NSInitialToolTipDelay -integer 0
+defaults write -g NSWindowResizeTime 0.1 
+defaults write -g QLPanelAnimationDuration -float 0.15
+defaults write NSGlobalDomain AppleShowScrollBars -string "Always"
+defaults write NSGlobalDomain InitialKeyRepeat -int 9
+defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool NO
+defaults write NSGlobalDomain com.apple.springing.delay -float 0
+defaults write NSGlobalDomain com.apple.springing.enabled -bool true
+defaults write com.apple.CrashReporter DialogType -string "none"
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+defaults write com.apple.dashboard mcx-disabled -bool true
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.dock autohide-delay -float 0
+defaults write com.apple.dock autohide-time-modifier -float 0
+defaults write com.apple.dock expose-animation-duration -float 0
+defaults write com.apple.dock mineffect suck
+defaults write com.apple.finder AnimateInfoPanes -boolean false
+defaults write com.apple.finder AnimateWindowZoom -bool false
+defaults write com.apple.finder AppleShowAllFiles YES
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false 
+defaults write com.apple.finder QLEnableTextSelection -bool true
+defaults write com.apple.finder QLHidePanelOnDeactivate -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder ShowTabView -bool true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true 
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+defaults write com.apple.screencapture disable-shadow -bool true
+defaults write com.apple.screencapture type -string "jpg"
+defaults write com.apple.terminal StringEncodings -array 4
+echo "$(tput setaf 2)Initialize defaults settings complete! :)$(tput sgr0)"
 
 exit 0
